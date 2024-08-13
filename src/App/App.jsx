7f107f0adb2f517ced12271800e4b80c87ac9acb
RafaIbarra <blasrafael1986@gmail.com>
2024-08-13 11:15:41 -0400

    inicio del proyecto

---
+++ src/App/App.jsx
@@ -0,0 +1,12 @@
+import React,{useState,useEffect} from "react";
+function App(){
+
+
+    return(
+        <div>
+            Hola mi nombre es Rafael
+        </div>
+    )
+}
+
+export default App
